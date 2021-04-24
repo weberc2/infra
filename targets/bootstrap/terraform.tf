02ddac1c7affe7b2f0bc58810c13de8a12037be6
Craig Weber <weberc2@gmail.com>
2021-04-24 09:59:32 -0500

    #2: Upgrading tf version because 0.12 is too low for GH action support

--- targets/bootstrap/terraform.tf
+++ targets/bootstrap/terraform.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hashicorp/aws"
+      version = "~> 3.37.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
